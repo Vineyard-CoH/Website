eed23ed99f3e4bb0ebeaf6c13323da8dd598efb8
Vineyard-CoH <135942356+Vineyard-CoH@users.noreply.github.com>
2023-06-25 15:58:27 -0400

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,18 @@
+const translations = {
+    English: {
+      "I Need Food": "I Need Food",
+      "I Want to Help": "I Want to Help",
+      "Who We Are": "Who We Are"
+    },
+    Português: {
+      "I Need Food": "Eu Preciso de Comida",
+      "I Want to Help": "Eu Quero Ajudar",
+      "Who We Are": "Quem Somos"
+    }
+  };
+  
+  // Additional translation keys can be dynamically added
+  translations.English["Contact Us"] = "Contact Us";
+  translations.Português["Contact Us"] = "Entre em Contato";
+  
+  export default translations;
