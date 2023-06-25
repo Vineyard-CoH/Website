270f74c341a7e0e935e24a80c6e867f817300d7c
Vineyard-CoH <135942356+Vineyard-CoH@users.noreply.github.com>
2023-06-25 14:51:34 -0400

    Update translation.js

--- translation.js
+++ translation.js
@@ -1,26 +1,26 @@
 function changeLanguage() {
-  var languageSwitcher = document.getElementById("language-switcher");
-  var button = languageSwitcher.querySelector("button");
-  var currentLanguage = button.textContent;
+    var languageSwitcher = document.getElementById("language-switcher");
+    var button = languageSwitcher.querySelector("button");
+    var currentLanguage = button.textContent;
 
-  var foodLink = document.querySelector("#navigation a[href='food.html']");
-  var helpLink = document.querySelector("#navigation a[href='help.html']");
-  var whoLink = document.querySelector("#navigation a[href='who.html']");
-  var footerText = document.querySelector("footer");
+    var foodLink = document.getElementById("food-link");
+    var helpLink = document.getElementById("help-link");
+    var whoLink = document.getElementById("who-link");
+    var footerText = document.querySelector("footer");
 
-  var translations;
+    var translations;
 
-  if (currentLanguage === "Português") {
-      // Change text to Portuguese
-      button.textContent = "English";
-      translations = translations_pt;
-  } else {
-      // Change text to English
-      button.textContent = "Português";
-      translations = translations_en;
-  }
+    if (currentLanguage === "Português") {
+        // Change text to Portuguese
+        button.textContent = "English";
+        translations = translations_pt;
+    } else {
+        // Change text to English
+        button.textContent = "Português";
+        translations = translations_en;
+    }
 
-  foodLink.textContent = translations["I Need Food"];
-  helpLink.textContent = translations["I Want to Help"];
-  whoLink.textContent = translations["Who We Are"];
+    foodLink.textContent = translations["I Need Food"];
+    helpLink.textContent = translations["I Want to Help"];
+    whoLink.textContent = translations["Who We Are"];
 }
