ab625be58467beccf54088fcd04be9cd1fc93f00
Vineyard-CoH <135942356+Vineyard-CoH@users.noreply.github.com>
2023-06-25 15:05:56 -0400

    Update translation.js

--- translation.js
+++ translation.js
@@ -1,26 +1,37 @@
+var translations = translations_en;
+
+function translateElements() {
+    var elements = document.querySelectorAll("[data-translate-key]");
+    
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var translateKey = element.getAttribute("data-translate-key");
+        
+        if (translations.hasOwnProperty(translateKey)) {
+            element.textContent = translations[translateKey];
+        }
+    }
+}
+
 function changeLanguage() {
     var languageSwitcher = document.getElementById("language-switcher");
     var button = languageSwitcher.querySelector("button");
     var currentLanguage = button.textContent;
 
-    var foodLink = document.getElementById("food-link");
-    var helpLink = document.getElementById("help-link");
-    var whoLink = document.getElementById("who-link");
-    var footerText = document.querySelector("footer");
-
-    var translations;
-
     if (currentLanguage === "Português") {
-        // Change text to Portuguese
-        button.textContent = "English";
-        translations = translations_pt;
-    } else {
         // Change text to English
         button.textContent = "Português";
         translations = translations_en;
+    } else {
+        // Change text to Portuguese
+        button.textContent = "English";
+        translations = translations_pt;
     }
 
-    foodLink.textContent = translations["I Need Food"];
-    helpLink.textContent = translations["I Want to Help"];
-    whoLink.textContent = translations["Who We Are"];
+    translateElements();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Set default language to English
+    changeLanguage();
+});
